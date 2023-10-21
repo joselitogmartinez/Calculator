fec763233cb9c64ea35bd657bca1017d378e4c47
Joselito Giron <66401888+joselitogmartinez@users.noreply.github.com>
2023-10-21 09:24:28 -0600

    cuarto commit

--- Calculator.py
+++ Calculator.py
@@ -11,9 +11,14 @@ class Calculator:
         sub = self.num1 - self.num2
         print("El resultado de la resta es: ", sub)
 
+    def Multiplication(self):
+        mul = self.num1 * self.num2
+        print("El resultado de la multiplicación es: ", mul)
+
 num1 = input("Ingrese el primer número: ")
 num2 = input("Ingrese el segundo número: ")
 
 calculator = Calculator(num1, num2)
 calculator.Addition()
 calculator.Substraction()
+calculator.Multiplication()
